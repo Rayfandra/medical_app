206c4fcda6cbe653931e30a57132d3a0ffbb050a
Rayfandra <rayfandra.devano.2003@gmail.com>
2023-11-28 16:38:42 +0700

    first commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,88 @@
+import 'package:cleaning_servis_app/color_schema.dart';
+import 'package:cleaning_servis_app/main_page.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: deepPurple400,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const SizedBox(
+            height: 60,
+          ),
+          const Text(
+            "Cleaning Service\nOnline",
+            style: TextStyle(
+              fontSize: 40.0,
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          const Text(
+            "Rumah Sehat\nuntuk\nKehidupan yang Sehat",
+            style: TextStyle(
+              fontSize: 20.0,
+              color: Colors.white,
+              fontWeight: FontWeight.w500,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          const SizedBox(
+            height: 20.0,
+          ),
+          Container(
+            height: 350,
+            width: MediaQuery.of(context).size.width,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/image/splash.png'),
+                  fit: BoxFit.cover),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              InkWell(
+                onTap: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return const MainPage();
+                    },
+                  ),
+                ),
+                child: Container(
+                  padding: const EdgeInsets.symmetric(
+                    horizontal: 60,
+                    vertical: 30,
+                  ),
+                  decoration: const BoxDecoration(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                    ),
+                    color: Colors.white,
+                  ),
+                  child: Text(
+                    "Lanjutkan",
+                    style: TextStyle(
+                        fontSize: 17.0,
+                        fontWeight: FontWeight.w600,
+                        color: deepPurple400),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
